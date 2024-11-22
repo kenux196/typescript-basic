04f7cc9d151b26db381ddbc005ef4d263e0eff1e
kenux196 <kenux.yun@gmail.com>
2024-11-22 14:46:05 +0900

    add user sample

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+import User from './user/user';
+
 console.log('Hello, Typescript~~~');
+
+const user: User = new User('kenux', 47);
+
+console.log(`${user.getName()}의 나이는 ${user.getAge()} 입니다.`);
---
+++ src/user/user.ts
@@ -0,0 +1,19 @@
+class User {
+  private name: string;
+  private age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getAge() {
+    return this.age;
+  }
+}
+
+export default User;
